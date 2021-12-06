1106f3cd49d1645b84319ddf82dde9424a65da5c
Kyle Bailey <kylealexbailey@gmail.com>
2021-12-07 02:36:32 +0900

    set VideoCapture to -1,2

--- scanbarcode.py
+++ scanbarcode.py
@@ -23,7 +23,7 @@ def read_barcodes(frame):
 
 
 def start_scan():
-    camera = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(-1,2)
     try:
         while True:
             scanning, frame = camera.read()
